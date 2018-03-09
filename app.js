15cd69676568d5c6ba86565db69a34cc25eaa1f6
Carlos Robayo <charlie.roca@hotmail.com>
2018-03-09 18:50:33 -0500

    Init de project

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+
+var app = express();
+
+const port = process.env.port || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('*', function(req, res){
+
+    if (req.url == '/') {
+        res.sendFile('index.html');
+    }
+
+});
+
+app.listen(port);
